d7e75163b253975e43fb13152adc6f7501f454f6
16523172 Muhammad Zia Alby <167898369+Joyjioyy@users.noreply.github.com>
2024-12-19 13:41:05 +0700

    Update AddandSub.vhd ke 16 bit

--- AddandSub.vhd
+++ AddandSub.vhd
@@ -4,10 +4,10 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity AddSub is
     Port (
-        A       : in  signed(7 downto 0); -- Bilangan yang akan dikurangi atau ditambah
-        B       : in  signed(7 downto 0); -- Bilangan yang akan mengurangi atau menambah (didapat dari fungsi cordic)
+        A       : in  signed(15 downto 0); -- Bilangan yang akan dikurangi atau ditambah
+        B       : in  signed(15 downto 0); -- Bilangan yang akan mengurangi atau menambah (didapat dari fungsi cordic)
         add_sub : in  std_logic;           -- Kontrol: '0' untuk penjumlahan, '1' untuk pengurangan. Ini nanti didapat dari hasil komparator
-        Result  : out signed(7 downto 0) -- Hasil operasi Q3.13 ((1 sign, 2 integer, 13 fraction dengan asumsi input di rentang -1 sampai 1)
+        Result  : out signed(15 downto 0) -- Hasil operasi Q3.13 ((1 sign, 2 integer, 13 fraction dengan asumsi input di rentang -1 sampai 1)
     );
 
     -- Catatan input 1 sign 2 integer 5 fraction. Input maksimal bernilai dari -1 sampai 1
