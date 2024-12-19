dddbbce17cb78c461cd2d5ef80a8528efe53ff95
16523172 Muhammad Zia Alby <167898369+Joyjioyy@users.noreply.github.com>
2024-12-19 13:41:34 +0700

    Update comparatortogetsigma.vhd ke 16 bit

--- comparatortogetsigma.vhd
+++ comparatortogetsigma.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity comparator is
     Port (
-        Parameter       : in  signed(7 downto 0); -- Bilangan yang akan dikomparisasi
+        Parameter       : in  signed(15 downto 0); -- Bilangan yang akan dikomparisasi
         Result  : out std_logic -- Hasil operasi Q3.13 ((1 sign, 2 integer, 13 fraction dengan asumsi input di rentang -1 sampai 1)
     );
 end comparator;
@@ -12,14 +12,5 @@ end comparator;
 architecture Behavioral of comparator is
 begin
 
-    Result <= not(Parameter(7));
-    -- process(Parameter)
-    -- begin
-    --     if Parameter(7) = '0' then -- jika bilangan positif
-    --         Result <= '1';
-    --     else
-    --         -- Jika bilangan negatif
-    --         Result <= '0';
-    --     end if;
-    -- end process;
+    Result <= not(Parameter(15));
 end Behavioral;
